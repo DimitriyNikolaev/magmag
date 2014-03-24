a231d240548f16119518503f0e7c70b9bab2d6e0
dimitriy <constructor3@yandex.ru>
2014-03-25 01:23:10 +0400

    product page basket

--- magmag_core/static/magmag_core/js/basket/basket.js
+++ magmag_core/static/magmag_core/js/basket/basket.js
@@ -3,6 +3,13 @@
  */
 
 function Basket() {
+
+    var tmpl = '<div id="purchase_item_{0}" class="basket-item">' +
+                '<a href="{1}">{2}</a>' +
+                '<img src="{3}" />' +
+                '<div><strong id="pi_count_{0}">{4}</strong> x <strong>{5}</strong></div>' +
+                '</div>';
+
     this.checkSupportHtml5Storage = function (){
         try {
             return 'localStorage' in window && window['localStorage'] !== null;
@@ -24,13 +31,21 @@ function Basket() {
       window.attachEvent("onstorage", this.handle_storage);
     }
 
-    this.init = function(store_name, container, counter){
+    this.init = function(store_name, container, counter, total_sum){
         this.prefix = store_name+'_pi_';
         this.container = container;
         this.counter = counter;
+        this.total_sum = total_sum
+        this.updateContainer();
     };
 
     this.setPurchaseItem = function(id, data){
+        var existItem = this.getPurchaseItem(id);
+        if(existItem != null)
+        {
+            data.count = existItem.count + 1;
+        }
+        else data.count = 1;
         localStorage.setItem(this.prefix+id, JSON.stringify(data))
         this.updateContainer();
     };
@@ -69,7 +84,25 @@ function Basket() {
     }
 
     this.updateContainer = function(){
-      alert('updated');
+        var items = this.getPIList();
+        var totalcount = 0;
+        var totalsumm = 0;
+        for(var i = 0; i < items.length; i++){
+            var item = items[i];
+            totalcount += item.count;
+            totalsumm += item.count*item.price;
+            if ($('#purchase_item_'+item.id).length > 0){
+                if($('#pi_count_'+item.id).text() != item.count+'')
+                    $('#pi_count_'+item.id).text(item.count);
+            }
+            else{
+                var block = $(tmpl.format(item.id, '#',item.product, item.image, item.count, item.price));
+                this.container.append(block);
+            }
+        }
+        this.counter.text(totalcount);
+        this.total_sum.text(totalsumm);
+
     };
 }
 
---
+++ magmag_core/static/magmag_core/js/utils.js
@@ -0,0 +1,12 @@
+/**
+ * Created by dimitriy on 23.03.14.
+ */
+
+String.prototype.format = function() {
+  var s = this;
+  for (var i = 0; i < arguments.length; i++) {
+    var reg = new RegExp("\\{" + i + "\\}", "gm");
+    s = s.replace(reg, arguments[i]);
+  }
+  return s;
+}
